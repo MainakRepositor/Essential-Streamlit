def0df937f55a2869a828420ee9d4ff39137f64d
MAINAK CHAUDHURI <sultankhilji001@gmail.com>
2023-06-06 22:53:11 +0530

    Add files via upload

--- data-element.py
+++ data-element.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import random
+from datetime import datetime
 
 df = pd.DataFrame(
    np.random.randn(50, 20),
@@ -37,3 +37,176 @@ df = load_data()
 st.dataframe(df, use_container_width=st.session_state.use_container_width)
 
 #=====================================================
+
+st.subheader('Dataframe with checkboxes')
+
+df = pd.DataFrame(
+    [
+        {"command": "st.selectbox", "rating": 4, "is_widget": True},
+        {"command": "st.balloons", "rating": 5, "is_widget": False},
+        {"command": "st.time_input", "rating": 3, "is_widget": True},
+    ]
+)
+
+
+
+st.dataframe(df, use_container_width=True)
+
+data_df = pd.DataFrame(
+    {
+        "widgets": ["st.selectbox", "st.number_input", "st.text_area", "st.button"],
+    }
+)
+
+st.divider()
+
+st.subheader('Dataframe with editable columns')
+
+st.data_editor(
+    data_df,
+    column_config={
+        "widgets": st.column_config.Column(
+            "Streamlit Widgets",
+            help="Streamlit **widget** commands 🎈",
+            width="medium",
+            required=True,
+        )
+    },
+    hide_index=True,
+    num_rows="dynamic",
+)
+st.divider()
+
+# checkbox and columns editables
+st.subheader('checkbox and columns editables')
+data_df = pd.DataFrame(
+    {
+        "widgets": ["st.selectbox", "st.number_input", "st.text_area", "st.button"],
+        "favorite": [True, False, False, True],
+    }
+)
+
+st.data_editor(
+    data_df,
+    column_config={
+        "favorite": st.column_config.CheckboxColumn(
+            "Your favorite?",
+            help="Select your **favorite** widgets",
+            default=False,
+        )
+    },
+    disabled=["widgets"],
+    hide_index=True,
+)
+
+st.divider()
+
+st.subheader('Select Box columns')
+
+data_df = pd.DataFrame(
+    {
+        "category": [
+            "📊 Data Exploration",
+            "📈 Data Visualization",
+            "🤖 LLM",
+            "📊 Data Exploration",
+        ],
+    }
+)
+
+st.data_editor(
+    data_df,
+    column_config={
+        "category": st.column_config.SelectboxColumn(
+            "App Category",
+            help="The category of the app",
+            width="medium",
+            options=[
+                "📊 Data Exploration",
+                "📈 Data Visualization",
+                "🤖 LLM",
+            ],
+        )
+    },
+    hide_index=True,
+)
+
+
+st.divider()
+
+st.subheader('Datetime columns')
+
+data_df = pd.DataFrame(
+    {
+        "appointment": [
+            datetime(2024, 2, 5, 12, 30),
+            datetime(2023, 11, 10, 18, 0),
+            datetime(2024, 3, 11, 20, 10),
+            datetime(2023, 9, 12, 3, 0),
+        ]
+    }
+)
+
+st.data_editor(
+    data_df,
+    column_config={
+        "appointment": st.column_config.DatetimeColumn(
+            "Appointment",
+            min_value=datetime(2023, 6, 1),
+            max_value=datetime(2025, 1, 1),
+            format="D MMM YYYY, h:mm a",
+            step=60,
+        ),
+    },
+    hide_index=True,
+)
+st.divider()
+
+st.subheader('Line chart columns')
+data_df = pd.DataFrame(
+    {
+        "sales": [
+            [0, 4, 26, 80, 100, 40],
+            [80, 20, 80, 35, 40, 100],
+            [10, 20, 80, 80, 70, 0],
+            [10, 100, 20, 100, 30, 100],
+        ],
+    }
+)
+
+st.data_editor(
+    data_df,
+    column_config={
+        "sales": st.column_config.LineChartColumn(
+            "Sales (last 6 months)",
+            width="medium",
+            help="The sales volume in the last 6 months",
+            y_min=0,
+            y_max=100,
+         ),
+    },
+    hide_index=True,
+)
+st.divider()
+
+st.subheader('Progress bar columns')
+
+data_df = pd.DataFrame(
+    {
+        "sales": [200, 550, 1000, 80],
+    }
+)
+
+st.data_editor(
+    data_df,
+    column_config={
+        "sales": st.column_config.ProgressColumn(
+            "Sales volume",
+            help="The sales volume in USD",
+            format="$%f",
+            min_value=0,
+            max_value=1000,
+        ),
+    },
+    hide_index=True,
+)
